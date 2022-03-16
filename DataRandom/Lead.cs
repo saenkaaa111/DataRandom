b4d47509437bf0c5ae69ec939c4c6dc88d7892b4
Valeriia <portnovavalerija@mail.ru>
2022-03-16 19:10:34 +0300

    fix for accounts (rub)

--- DataRandom/Lead.cs
+++ DataRandom/Lead.cs
@@ -4,14 +4,6 @@ namespace DataRandom
     public class Lead
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string LastName { get; set; }
-        public DateTime BirthDate { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Password { get; set; }
-        public string City { get; set; }
         public int Role { get; set; }
-        public bool IsBanned { get; set; }
     }
 }
